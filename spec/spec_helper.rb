5fdf8138917a313df32c3091b2ee00a6c5e36f88
maxwellv <max.vance@gmail.com>
2014-06-11 14:31:02 -0500

    added SimpleCov reporting stuff

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,15 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
 require 'simplecov'
+
+
+formatters =  [SimpleCov::Formatter::HTMLFormatter]
+formatters << CodeClimate::TestReporter::Formatter if ENV['CODECLIMATE_REPO_TOKEN']
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter [*formatters]
 SimpleCov.start
+
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
